a9cb383d85da8a3fd69713968c7df7fe5d04e078
Ricardo Zaragoza <ricardo.zaragoza@pnc.com>
2020-02-02 14:18:26 -0500

    Files configs

---
+++ src/main/java/com/mulrica/jib/JibApplication.java
@@ -0,0 +1,13 @@
+package com.mulrica.jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulrica/jib/JibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulrica.jib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
